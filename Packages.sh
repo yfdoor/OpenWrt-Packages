5b29acb229d0bc4ab50172a3f9cafddd9c9015a9
YFDOOR <32769281+yfdoor@users.noreply.github.com>
2022-02-26 16:07:39 +0800

    Update Packages.sh

--- Packages.sh
+++ Packages.sh
@@ -4,7 +4,7 @@ rm -rf lean lienol yfdoor
 svn co https://github.com/coolsnowwolf/luci/trunk/applications                          lean
 svn co https://github.com/Lienol/openwrt-package/trunk                                  lienol
 svn co https://github.com/fw876/helloworld/trunk                                        yfdoor
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/default-settings         yfdoor/default-settings
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean                          yfdoor
 svn co https://github.com/rufengsuixing/luci-app-adguardhome/trunk                      yfdoor/luci-app-adguardhome
 
 rm -rf ./lean/default-settings
