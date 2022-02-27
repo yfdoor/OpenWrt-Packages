b9e86cecf10f6d05813d6c022b34d17d9e4abdae
YFDOOR <32769281+yfdoor@users.noreply.github.com>
2022-02-27 09:41:05 +0800

    Update Packages.sh

--- Packages.sh
+++ Packages.sh
@@ -4,17 +4,11 @@ mv -n `find $1/* -maxdepth 0 -type d` ./
 rm -rf $1
 }
 
-#svn co https://github.com/coolsnowwolf/luci/trunk/applications                          lean
-#svn co https://github.com/Lienol/openwrt-package/trunk                                  lienol
-#svn co https://github.com/fw876/helloworld/trunk                                        yfdoor/helloworld
-#svn co https://github.com/rufengsuixing/luci-app-adguardhome/trunk                      yfdoor/luci-app-adguardhome
-
 git clone --depth 1 https://github.com/kenzok8/openwrt-packages                         kenzok
 git clone --depth 1 https://github.com/kenzok8/small                                    dependency
-git clone --depth 1 https://github.com/fw876/helloworld/trunk                           yfdoor/helloworld
+#git clone --depth 1 https://github.com/fw876/helloworld                                 yfdoor/helloworld
 
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/default-settings         yfdoor/default-settings 
-
 
 rm -rf ./*/.git & rm -f ./*/.gitattributes
 rm -rf ./*/.svn & rm -rf ./*/.github & rm -rf ./*/.gitignore
