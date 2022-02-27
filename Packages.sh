63f2b5aa2563109ee7ce934ff232fd59b9c56a24
YFDOOR <32769281+yfdoor@users.noreply.github.com>
2022-02-27 09:17:24 +0800

    Update Packages.sh

--- Packages.sh
+++ Packages.sh
@@ -6,7 +6,7 @@ rm -rf kenzok dependency yfdoor
 #svn co https://github.com/fw876/helloworld/trunk                                        yfdoor/helloworld
 #svn co https://github.com/rufengsuixing/luci-app-adguardhome/trunk                      yfdoor/luci-app-adguardhome
 
-svn co https://github.com/kenzok8/openwrt-packages/trunk                                kenzok
+#svn co https://github.com/kenzok8/openwrt-packages/trunk                                kenzok
 #svn co https://github.com/kenzok8/small/trunk                                           dependency
 #svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/default-settings         yfdoor/default-settings 
 #svn co https://github.com/fw876/helloworld/trunk                                        yfdoor/helloworld
