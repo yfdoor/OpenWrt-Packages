17a5e069a16465a6253a8ea3931c343edf2d1838
YFDOOR <32769281+yfdoor@users.noreply.github.com>
2023-01-11 20:41:21 +0800

    Update Packages.sh

--- Packages.sh
+++ Packages.sh
@@ -4,10 +4,12 @@ rm -rf kenzok dependency yfdoor
 git clone --depth 1 https://github.com/kenzok8/openwrt-packages                         kenzok
 git clone --depth 1 https://github.com/kenzok8/small                                    dependency
 
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean                          lean
 svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/default-settings         yfdoor/default-settings 
 
 rm -rf ./*/.git & rm -rf ./*/*/.git & rm -rf ./*/*/*/.git
 rm -rf ./*/.gitignore & rm -rf ./*/*/.gitignore & rm -rf ./*/*/*/.gitignore
 rm -rf ./*/.svn & rm -rf ./*/*/.svn & rm -rf ./*/*/*/.svn
+rm -rf ./lean/default-settings
 
 exit 0
